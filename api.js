aed74cc6c8c6dff81074675999ce60802227c120
Deivecito <agustingonzalez200133@gmail.com>
2023-09-03 21:45:37 -0300

    first commit

--- api.js
+++ api.js
@@ -1,8 +1,23 @@
-fetch('https://rickandmortyapi.com/api/character/1')
+ID = 1;
+while (ID < 100) {
+fetch(`https://rickandmortyapi.com/api/character/${ID}`)
   .then(response => response.json()
-  .then(data => { document.getElementById('name').innerHTML = `Nombre: ${data.name} <br> Estado: ${data.status} <br> Especie: ${data.species}`;
-  })
+  .then(data => { const charactersContainer = document.getElementById('characters-container');
+  
+  const characterColumn = document.createElement('div');
+  characterColumn.classList.add('col');
+
+  characterColumn.innerHTML = `
+    <p><img class="d-block w-1" src="${data.image}" style="position: flex; "> ${data.name} <br> ${data.status} <br> ${data.species} </p>
+  `;
+  
+  charactersContainer.appendChild(characterColumn);
+
+})
   .catch(error => {
     
     console.error('Ocurrió un error:', error);
   }))
+
+  ID ++;
+}
